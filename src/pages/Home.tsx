5f9239e4e25af6f9ccc2f36657b3d57900352e77
lsh <3040114965@qq.com>
2024-04-13 20:15:00 +0800

    完成动态菜单

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,51 @@
+import { useState } from "react";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+import { Layout, Button, theme } from "antd";
+import LeftMenu from "../component/LeftMenu";
 
-export default function Home() {
-  return <div>Home</div>;
+const { Header, Sider, Content } = Layout;
+
+function Home() {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <div>
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="demo-logo-vertical" />
+          <LeftMenu />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }}>
+            <Button
+              type="text"
+              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+              onClick={() => setCollapsed(!collapsed)}
+              style={{
+                fontSize: "16px",
+                width: 64,
+                height: 64,
+              }}
+            />
+          </Header>
+          <Content
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              minHeight: 280,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    </div>
+  );
 }
+
+export default Home;
